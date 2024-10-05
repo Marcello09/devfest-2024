4e2682ad5af87d0335ec7c4bd786ae8fb7ca43a7
Marcello Moreira <m.moreira@aeroscan.com.br>
2024-10-04 21:59:39 -0300

    Add Firebase and FirebaseUI dependencieAdd Sign In page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from "./shared/toolbar/toolbar.component";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +15,27 @@ import { ToolbarComponent } from "./shared/toolbar/toolbar.component";
 })
 export class AppComponent {
   title = 'frontend';
+
+
+  constructor() {
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+    
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyD98JsuXE1w7ainrosODVYeH3E2WbsFrjI",
+      authDomain: "moreira-devfest-2024.firebaseapp.com",
+      projectId: "moreira-devfest-2024",
+      storageBucket: "moreira-devfest-2024.appspot.com",
+      messagingSenderId: "252847206578",
+      appId: "1:252847206578:web:cf4d55cdcd964ec34e99ab",
+      measurementId: "G-WBS3W6S0XD"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+  }
 }
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import * as firebaseui from 'firebaseui';
+import { getAuth, GoogleAuthProvider, getRedirectResult } from 'firebase/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule, 
+    MatInputModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatButtonModule, 
+    CommonModule, 
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+
+  hide = true;
+
+  onGoogleSignIn = function (googleUser: any) {
+    // Handle the Google One Tap sign-in response
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+  }
+
+  constructor(private router: Router) {
+    getRedirectResult(getAuth()).then((result) => {
+      console.log(result)
+      if (result) {
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+  
+  ngAfterViewInit(): void {
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(getAuth());
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        GoogleAuthProvider.PROVIDER_ID
+      ],
+      signInFlow: 'popup',
+      signInSuccessUrl: 'home',
+    });
+  }
 
 }
