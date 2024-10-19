d544b4a14e085a140c8eb2158ecb472514be3c43
Marcello Moreira <m.moreira@aeroscan.com.br>
2024-10-19 11:02:12 -0300

    PWA

--- frontend/src/app/shared/toolbar/toolbar.component.ts
+++ frontend/src/app/shared/toolbar/toolbar.component.ts
@@ -3,6 +3,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
@@ -17,5 +18,13 @@ import { RouterModule } from '@angular/router';
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+
+  constructor(private location: Location) {
+
+  }
+
+  goBack() {
+    this.location.back()
+  }
 
 }
